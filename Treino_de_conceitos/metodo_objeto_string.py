a353529bc891d7a633f011ee86f59f4b2ffac7e3
Rodrigo Galvão <rodrigogalvao136@gmail.com>
2024-04-25 23:48:08 -0300

    Update: adicionando testes de método de objeto de string, além de também testar formatação de texto e "escape"

---
+++ Treino_de_conceitos/metodo_objeto_string.py
@@ -0,0 +1,16 @@
+nome = "João"
+idade = 30
+altura = 1.75
+print("Olá, meu nome é {} e tenho {} anos. Minha altura é {:.2f} metros.".format(nome, idade, altura))
+
+nome = "João"
+idade = 30
+print("Olá, meu nome é {} e tenho {} anos.".format(nome, idade))
+
+numero = 3.14159
+print("{:.2f}".format(numero))
+
+nome = "João"
+idade = 30
+print("Olá, meu nome é {nome} e tenho {idade} anos.".format(nome=nome, idade=idade))
+
--- teste_generico.py
+++ teste_generico.py
@@ -1,1 +1,22 @@
-#teste de feature
+#teste de "escape" (Controle de saída do texto)
+print("\n Olá")
+print(" não entendi \r Olá")
+print(" quem es tu\" Olá")
+print("que \\ Olá")
+print(" qual foi \b Olá")
+print("oi \f Olá")
+print("\t Olá")
+
+#testes de formatação
+
+num = 12
+dec = 7.7
+
+print("%d" % 7)
+print("%f" % 9)
+print("%e" % 7.6)
+print("%o" % 7)
+print("%x" % 76)
+
+
+
